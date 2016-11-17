cd5fe64e33c972821b5aa645a0a4bf66805d8848
chkui <chenkui@chkui.com>
2016-11-17 16:42:02 +0800

    add code

---
+++ entry.js
@@ -0,0 +1,8 @@
+/**
+ * Created by chkui on 2016/11/16.
+ */
+define(function(require, exports, module) {
+    require("!style!css!./style.css");
+    document.write('<h1>hello webasdsadpack!!!ssd!!</h1>');
+    document.write(require('./module.js'));
+});
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by chkui on 2016/11/16.
+ */
+module.exports = {
+    entry: './entry.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: 'style!css'}
+        ]
+    }
+}
