f0d4fc5953281b7ecbfcc07f63015e1d552594db
chkui <chenkui@chkui.com>
2016-11-17 17:28:56 +0800

    modify js show text

--- entry.js
+++ entry.js
@@ -1,8 +1,10 @@
 /**
  * Created by chkui on 2016/11/16.
  */
+
+//requirejs AMD
 define(function(require, exports, module) {
     require("!style!css!./style.css");
-    document.write('<h1>hello webasdsadpack!!!ssd!!</h1>');
+    document.write('<h1>hello react!</h1>');
     document.write(require('./module.js'));
 });
--- module.js
+++ module.js
@@ -1,4 +1,4 @@
 /**
  * Created by chkui on 2016/11/16.
  */
-module.exports = 'It works from module.js.!!'
+module.exports = 'It works from module.js.'
